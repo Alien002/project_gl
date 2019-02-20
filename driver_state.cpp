50be530539a3a5594bc1f179daeb38f79a01fca7
Alic Lien <Alien002@ucr.edu>
2019-02-20 05:53:18 -0800

    mem leak test

--- driver_state.cpp
+++ driver_state.cpp
@@ -44,8 +44,8 @@ void render(driver_state& state, render_type type)
 {
     std::cout<<"TODO: implement rendering."<<std::endl;
     
-    auto *triangle = new data_geometry[3];
-    auto ptr = state.vertex_data;
+    data_geometry *triangle = new data_geometry[3];
+    float ptr = state.vertex_data;
     data_vertex in;
     
     
