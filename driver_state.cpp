3d7ec2cc371d8b5a8c80bd0ddba1d8d6a60a2211
Alic Lien <Alien002@ucr.edu>
2019-03-04 01:09:51 -0800

    driver_state test

--- driver_state.cpp
+++ driver_state.cpp
@@ -22,10 +22,10 @@ void initialize_render(driver_state& state, int width, int height)
     state.image_depth=nullptr;
     //std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
     
-    unsigned long long total_pixel = width * height;           //check, might be okay with unsigned int (pixel)
-    state.image_color = new pixel[total_pixel];
+    //unsigned long long total_pixel = width * height;           //check, might be okay with unsigned int (pixel)
+    state.image_color = new pixel[width * height];
     
-    for(size_t i = 0; i < total_pixel; ++i){
+    for(size_t i = 0; i < width * height; ++i){
         
         state.image_color[i] = make_pixel(0,0,0);
         
