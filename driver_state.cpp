880f37e0c7bc71ae35729c44dbfb77b33ddcde23
Alic Lien <Alien002@ucr.edu>
2019-03-04 19:24:38 -0800

    driver_state test

--- driver_state.cpp
+++ driver_state.cpp
@@ -159,20 +159,19 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
             float beta = (0.5f * ((x[2] * y[0] - x[0] * y[2]) + (y[2] - y[0])*i + (x[0] - x[2])*j)) / area_abc;
             float gamma = (0.5f * ((x[0] * y[1] - x[1] * y[0]) + (y[0] - y[1])*i + (x[1] - x[0])*j)) / area_abc;
         
-            //float depth = (alpha * (*in)[0].gl_Position[2]) + (beta * (*in)[1].gl_Position[2]) + (gamma * (*in)[2].gl_Position[2]);
+            float depth = (alpha * (*in)[0].gl_Position[2]) + (beta * (*in)[1].gl_Position[2]) + (gamma * (*in)[2].gl_Position[2]);
 
             
-            if(alpha >= 0 && beta >= 0 && gamma >= 0 /*&& depth < state.image_depth[i + j * state.image_width]*/){
-                //const float alpha_p = alpha;
-                //const float beta_p = beta;
-                //const float gamma_p = gamma;
+            if(alpha >= 0 && beta >= 0 && gamma >= 0 /*&& depth <= state.image_depth[i + j * state.image_width]*/){
+                const float alpha_p = alpha;
+                const float beta_p = beta;
+                const float gamma_p = gamma;
                 
-                float depth = (alpha * (*in)[0].gl_Position[2]) + (beta * (*in)[1].gl_Position[2]) + (gamma * (*in)[2].gl_Position[2]);
-
+                /*
                 if(depth > state.image_depth[i + j * state.image_width]){
-                    //continue;
+                    continue;
                 }
-                
+                */
                 for(int k = 0; k < state.floats_per_vertex; ++k){
                     float k_gour;
                     
@@ -184,22 +183,22 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
                             break;
                         case interp_type::smooth:
                             
-                            k_gour = (alpha / (*in)[0].gl_Position[3])
-                                        + (beta / (*in)[1].gl_Position[3])
-                                        + (gamma / (*in)[2].gl_Position[3]);
+                            k_gour = (alpha_p / (*in)[0].gl_Position[3])
+                                        + (beta_p / (*in)[1].gl_Position[3])
+                                        + (gamma_p / (*in)[2].gl_Position[3]);
                             
                             
-                            alpha = alpha / (k_gour * (*in)[0].gl_Position[3]);
-                            beta = beta / (k_gour * (*in)[1].gl_Position[3]);
-                            gamma = gamma / (k_gour * (*in)[2].gl_Position[3]);
+                            alpha = alpha_p / (k_gour * (*in)[0].gl_Position[3]);
+                            beta = beta_p / (k_gour * (*in)[1].gl_Position[3]);
+                            gamma = gamma_p / (k_gour * (*in)[2].gl_Position[3]);
                             
                             //fragment_data.data[k] = alpha + beta + gamma;
-                            break;
+                            //break;
                         case interp_type::noperspective:
                             
-                            fragment_data.data[k] = alpha * (*in)[0].data[k]
-                                                    + beta * (*in)[1].data[k]
-                                                    + gamma * (*in)[2].data[k];
+                            fragment_data.data[k] = alpha_p * (*in)[0].data[k]
+                                                    + beta_p * (*in)[1].data[k]
+                                                    + gamma_p * (*in)[2].data[k];
                             
                             break;
                         default:
