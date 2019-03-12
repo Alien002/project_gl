a6846a401408bdb5bcecb02a43c575252ae6c819
Alic Lien <Alien002@ucr.edu>
2019-03-11 19:36:24 -0700

    testing

--- driver_state.cpp
+++ driver_state.cpp
@@ -227,7 +227,7 @@ void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
         clip_triangle(state,in3,face + 1);
         
         d2[0].data = new float[state.floats_per_vertex];
-        d2[2] = (*in)[2];
+        d2[2] = *in[2];
     
         for(int j = 0; j < state.floats_per_vertex; ++j){
             
