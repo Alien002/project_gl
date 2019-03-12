51b6e879bd59a8312c71b2daeee5b6df7ce7b605
Alic Lien <Alien002@ucr.edu>
2019-03-11 21:28:44 -0700

    Rasterize test

--- driver_state.cpp
+++ driver_state.cpp
@@ -424,15 +424,15 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
                 state.fragment_shader(fragment_data, output, state.uniform_data);
                 //out_data.output_color = out_data.output_color * 255;
                 
-                state.image_color[i + j * state.image_width] = make_pixel((out_data.output_color[0] * 255),
-                                                                          (out_data.output_color[1] * 255),
-                                                                          (out_data.output_color[2] * 255));
+                state.image_color[i + j * state.image_width] = make_pixel((output.output_color[0] * 255),
+                                                                          (output.output_color[1] * 255),
+                                                                          (output.output_color[2] * 255));
                 state.image_depth[i + j * state.image_width] = depth;
             }
         }
     }
     //std::cout<<"TODO: implement rasterization"<<std::endl;
-    delete [] data;
+ //   delete [] data;
 }
 
 
